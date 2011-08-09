4ea51fc4b08f2489cb4a69d76382befdca2adf33
aardjon <aardjon@e2eb7909-2e7c-48d3-89f5-df5953e6d4f3>
2011-08-09 04:59:59 +0000

    - Added GPL header to all source files

--- trunk/src/dao/IAbstractGameListWriter.hpp
+++ trunk/src/dao/IAbstractGameListWriter.hpp
@@ -1,22 +1,23 @@
-/* TODO
- *    <one line to give the program's name and a brief idea of what it does.>
- *    Copyright (C) 2011  thw <thomas@fomori.de>
+/*
+ * KBoy is a graphical user interface for managing a collection of Gameboy
+ * ROM files and assigning emulator configuration information to them.
  *
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
+ * Copyright (C) 2011  Thomas Wesenigk <thomas_aus_dresden@web.de>
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *    You should have received a copy of the GNU General Public License along
- *    with this program; if not, write to the Free Software Foundation, Inc.,
- *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
-
 
 #ifndef IABSTRACTGAMELISTWRITER_HPP
 #define IABSTRACTGAMELISTWRITER_HPP
